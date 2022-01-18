e8e4117425e0a7241e35cefd2979fae4e9e01d7a
Massurio <massurio@gmail.com>
2022-01-18 03:04:00 +0000

    removed log in state

--- client/src/Pages/Login.js
+++ client/src/Pages/Login.js
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
-
+import React from 'react';
+// { useState }
 export default function Login() {
-  const { username, setUsername } = useState('');
-  const { password, setPassword } = useState('');
+  //const { username, setUsername } = useState('');
+  //const { password, setPassword } = useState('');
 
   return (
     <div>
@@ -15,18 +15,18 @@ export default function Login() {
             name="username"
             title="username"
             placeholder="username"
-            onChange={(event) => {
-              setUsername(event.target.value);
-            }}
+            // onChange={(event) => {
+            //   setUsername(event.target.value);
+            // }}
           />
           <input
             type="password"
             name="password"
             title="password"
             placeholder="password"
-            onChange={(event) => {
-              setPassword(event.target.value);
-            }}
+            // onChange={(event) => {
+            //   setPassword(event.target.value);
+            // }}
           />
           <button type="submit" class="btn">
             Login
