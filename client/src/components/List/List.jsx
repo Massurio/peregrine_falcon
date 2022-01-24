c70e1c446b72e568d6256729f62c8abd0056265e
Massurio <massurio@gmail.com>
2022-01-24 11:51:11 +0000

    added location page functionality

---
+++ client/src/components/List/List.jsx
@@ -0,0 +1,85 @@
+import React, { useState, useEffect, createRef } from 'react'
+import {
+  CircularProgress,
+  Grid,
+  Typography,
+  InputLabel,
+  MenuItem,
+  FormControl,
+  Select,
+} from '@material-ui/core'
+
+import PlaceDetails from '../PlaceDetails/PlaceDetails'
+import useStyles from './styles.js'
+
+// fetch api for cards
+const List = ({
+  places,
+  childClicked,
+  isLoading,
+  type,
+  setType,
+  rating,
+  setRating,
+}) => {
+  const classes = useStyles()
+  const [elRefs, setElRefs] = useState([])
+
+  useEffect(() => {
+    const refs = Array(places?.length)
+      .fill()
+      .map((_, i) => elRefs[i] || createRef())
+    setElRefs(refs)
+  }, [places])
+
+  return (
+    <div className={classes.container}>
+      <Typography variant='h4'>
+        Restaurants, Accomodation and Attractions around you
+      </Typography>
+      {isLoading ? (
+        <div className={classes.loading}>
+          <CircularProgress size='5rem' />
+        </div>
+      ) : (
+        <>
+          <FormControl className={classes.formControl}>
+            <InputLabel>Type</InputLabel>
+            <Select value={type} onChange={(e) => setType(e.target.value)}>
+              <MenuItem value='restaurants'>Restaurants</MenuItem>
+              <MenuItem value='hotels'>Accommodation</MenuItem>
+              <MenuItem value='attractions'>Attractions</MenuItem>
+            </Select>
+          </FormControl>
+          <FormControl className={classes.formControl}>
+            <InputLabel id='rating'>Rating</InputLabel>
+            <Select
+              id='rating'
+              value={rating}
+              onChange={(e) => setRating(e.target.value)}
+            >
+              <MenuItem value='All'>All</MenuItem>
+              <MenuItem value='3'>Above 3.0</MenuItem>
+              <MenuItem value='4'>Above 4.0</MenuItem>
+              <MenuItem value='4.5'>Above 4.5</MenuItem>
+            </Select>
+          </FormControl>
+          {/* loop over places using map method */}
+          <Grid container spacing={3} className={classes.list}>
+            {places?.map((place, i) => (
+              <Grid item key={i} xs={12}>
+                <PlaceDetails
+                  place={place}
+                  selected={Number((childClicked = i))}
+                  refProp={elRefs[i]}
+                />
+              </Grid>
+            ))}
+          </Grid>
+        </>
+      )}
+    </div>
+  )
+}
+
+export default List
