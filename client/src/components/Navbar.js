b0604bb25a3e9df8f38b44324f1ede652a70911e
Massurio <massurio@gmail.com>
2022-01-27 12:49:54 +0000

    Nav bar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -71,7 +71,7 @@ export default function Navbar({ user, setUser }) {
                     className="nav-links"
                     onClick={closeMobileMenu}
                   >
-                    User Page
+                    UserPage
                   </Link>
                 </li>
               ) : (
