02f985efa1a22691b1a6c1da0cc43c0576ab50a9
LeeHol84 <92590336+LeeHol84@users.noreply.github.com>
2022-01-28 12:34:41 +0000

    contactus

--- src/Pages/ContactUs.js
+++ src/Pages/ContactUs.js
@@ -1,9 +1,89 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { signUpFetch } from '../utils';
+import { Link, useNavigate } from 'react-router-dom';
 
-export default function ContactUs() {
-  return (
-    <div>
-      <h1>Contact Us Page</h1>
-    </div>
-  );
-}
+
+  
+    
+    export default function ContactUs() {
+      const [username, setUsername] = useState('');
+      const [email, setEmail] = useState('');
+      const [passw, setpassw] = useState('');
+      // const [user, setUser] = useState('');
+      const [success, setSuccess] = useState(false);
+    
+    
+    
+      const handleSubmit = async (e) => {
+        e.preventDefault();
+        await signUpFetch(username, email, passw, setSuccess);
+        window.location.reload(false);
+        // console.log(user);
+      };
+    
+      const navigate = useNavigate();
+    
+      return (
+        <div className = "ContactUsBg">
+        <div className = 'form-wrapper'>
+        <div className="form">
+          <div className='loginTitile'></div>
+          
+          {!success ? (
+          <form onSubmit={handleSubmit}>
+            {/* <label alt="username"></label>
+            <input
+              type="text"
+              name="username"
+              title="username"
+              placeholder="username"
+              onChange={(e) => setUsername(e.target.value)}
+              required
+            />
+            <label alt="email"></label>
+            <input
+              type="email"
+              name="email"
+              title="email"
+              placeholder="email"
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            /> */}
+            {/* <label alt="password">Password</label>
+            <input
+              type="password"
+              name="password"
+              title="password"
+              placeholder="password"
+              onChange={(e) => setpassw(e.target.value)}
+              required
+            /> */}
+            <br />
+            <br />
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+                        {/* <p>{Acount made}</p> */}
+            {/* <button type="submit" className="button">
+              <Link to="/" />
+              
+            </button> */}
+          </form>): (navigate('/login'))}
+          
+        </div>
+        </div>
+        </div>
+        
+      );
+    }
+
